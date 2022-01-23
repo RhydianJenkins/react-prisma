3bfa0e0827381e5e89c7ca520271102e2308254d
Rhydian Jenkins <RhydianJenkins@users.noreply.github.com>
2022-01-23 17:33:09 +0000

    Webpack added and docker containerisation (#2)

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,9 @@
-# pull official base image
 FROM node:13.12.0-alpine
-
-# set working directory
+# ENV NODE_ENV=production
 WORKDIR /app
-
-# add `/app/node_modules/.bin` to $PATH
+COPY package*.json ./
 ENV PATH /app/node_modules/.bin:$PATH
-
-# install app dependencies
-COPY package.json ./
-COPY package-lock.json ./
-RUN npm install --silent
-RUN npm install react-scripts@3.4.1 -g --silent
-
-# add app
-COPY . ./
-
-# start app
+RUN npm install
+COPY . .
+EXPOSE 3000
 CMD ["npm", "start"]
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+const mode = 'development';
+
+module.exports = {
+  mode: mode,
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[fullhash].js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            cacheCompression: false,
+            envName: mode
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico'
+    })
+  ],
+  devServer: {
+    port: port
+  }
+};
